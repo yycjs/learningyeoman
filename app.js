136c0c4af8b0b8034171310240fa23919510682b
Sean Goresht <seangores@gmail.com>
2013-06-01 00:24:21 -0600

    Make version current, add live editing

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.configure("development", function ()
 app.get("/", routes.index);
 //app.get("/users", user.list);
 app.get('/edit-story', function(req, res){
-	res.render("plainstory", { title: "Drama 205: Final Project - Plain Story", description: "Interactive comic-style story-telling HTML5 application" });
+	res.render("plainstory", { title: "Drama 205: Final Project - Interactive, Plain Story", description: "Interactive comic-style story-telling HTML5 application" });
 });
 
 http.createServer(app).listen(app.get("port"), function ()
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
 
 exports.index = function (req, res)
 {
-	res.render("index", { title: "Drama 205: Final Project (Draft)", description: "Interactive comic-style story-telling HTML5 application" });
+	res.render("index", { title: "Drama 205: Final Project", description: "Interactive comic-style story-telling HTML5 application" });
 };
